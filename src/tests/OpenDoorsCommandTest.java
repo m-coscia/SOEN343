825570fa39c323f2216e49ea60b315d8ad5f2f44
yasminehilout <yasmine.hilout@gmail.com>
2024-03-14 17:57:34 -0400

    Added OpenDoorsCommand test

--- src/tests/OpenDoorsCommandTest.java
+++ src/tests/OpenDoorsCommandTest.java
@@ -4,17 +4,17 @@ import static org.junit.Assert.assertFalse;
 
 import org.junit.*;
 
-import src.commands.CloseDoorsCommand;
+import src.commands.OpenDoorsCommand;
 import src.components.Doors;
 
-public class CloseDoorsCommandTest {
+public class OpenDoorsCommandTest {
     // test method while having no doors, no users in the room and no caller to the method
     @Test 
     public void testCloseDoorsCommand() {
-        CloseDoorsCommand command = new CloseDoorsCommand(null, null, null);
+        OpenDoorsCommand command = new OpenDoorsCommand(null, null, null);
         Doors doors = new Doors(false);
-        doors.openDoorsCommand();
         doors.closeDoorsCommand();
-        assertFalse(doors.open);
+        doors.openDoorsCommand();
+        assertFalse(!doors.open);
     }
 }
