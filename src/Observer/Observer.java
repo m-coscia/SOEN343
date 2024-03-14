8947e05883d0363949d4090528e17eb855e4c18e
aloulidi <loulidiasmae@gmail.com>
2024-03-13 21:51:51 -0400

    RemoveAccountObserver start of the implementation

--- src/Observer/Observer.java
+++ src/Observer/Observer.java
@@ -2,6 +2,9 @@ package src.Observer;
 
 import src.logic.Profile;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public interface Observer {
-    public void update(Profile user);
+    public void update(Profile user) throws IOException;
 }
