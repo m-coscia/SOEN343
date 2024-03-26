908f1736d3435bf79e0ff76f4c4dcb3cf693a585
yasminehilout <yasmine.hilout@gmail.com>
2024-03-25 20:42:24 -0400

    Add throw exception

--- src/components/Component.java
+++ src/components/Component.java
@@ -1,5 +1,7 @@
 package src.components;
 
+import java.io.IOException;
+
 import src.commands.Command;
 
 public abstract class Component {
@@ -9,7 +11,7 @@ public abstract class Component {
         this.command = command;
     }
 
-    public void executeCommand(){
+    public void executeCommand() throws IOException{
         command.execute();
     }
 }
