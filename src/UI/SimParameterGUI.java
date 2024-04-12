e885db87958a8c3f60c0b7df5a72beba98c6ff71
yasminehilout <yasmine.hilout@gmail.com>
2024-04-12 15:26:50 -0400

    fix merge conflicts

--- src/UI/SimParameterGUI.java
+++ src/UI/SimParameterGUI.java
@@ -18,7 +18,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Properties;
 
-public class SimParameterGUI extends JFrame{
+public class SimParameterGUI extends JFrame {
     private Controller controller = Controller.getController();
     JFrame previousFrame = null;
     JFrame thisFrame = this;
@@ -45,44 +45,45 @@ public class SimParameterGUI extends JFrame{
     private ArrayList<JComboBox<String>> typesOfZonesList = new ArrayList<>();
 
     private Profile profile;
-    public SimParameterGUI(JFrame previousFrame, Profile profile){
+
+    public SimParameterGUI(JFrame previousFrame, Profile profile) {
         previousFrame = previousFrame;
         this.profile = profile;
         setUpButtons();
 
-        zoneCreationPanel.setLayout(new GridLayout(0,controller.getRooms().size() + 3));
+        zoneCreationPanel.setLayout(new GridLayout(0, controller.getRooms().size() + 3));
 
-        //empty for spacing
+        // empty for spacing
         JLabel empty = new JLabel("");
         zoneCreationPanel.add(empty);
 
-        //for every room type create a label for column
-        for (Room r: controller.getRooms()){
-            JLabel roomLabel = new JLabel(String.valueOf(r.getType()) + " (id: " + r.getId() +")");
+        // for every room type create a label for column
+        for (Room r : controller.getRooms()) {
+            JLabel roomLabel = new JLabel(String.valueOf(r.getType()) + " (id: " + r.getId() + ")");
             roomLabel.setHorizontalAlignment(SwingConstants.CENTER);
             zoneCreationPanel.add(roomLabel);
         }
 
-        //for spacing
+        // for spacing
         JLabel empty1 = new JLabel("Temperature");
         empty1.setHorizontalAlignment(SwingConstants.CENTER);
         zoneCreationPanel.add(empty1);
-        //empty1.setVisible(true);
+        // empty1.setVisible(true);
 
-        //for spacing
+        // for spacing
         JLabel typeLabel = new JLabel("Type of Zone");
         typeLabel.setHorizontalAlignment(SwingConstants.CENTER);
         zoneCreationPanel.add(typeLabel);
-        //typeLabel.setVisible(false);
+        // typeLabel.setVisible(false);
 
-        //TO choose type of zones
+        // TO choose type of zones
 
         JLabel zoneLabel = new JLabel("Zone 1");
         zoneLabel.setHorizontalAlignment(SwingConstants.CENTER);
         zoneCreationPanel.add(zoneLabel);
 
-        //add checkboxes for each room
-        for (Room r: controller.getRooms()){
+        // add checkboxes for each room
+        for (Room r : controller.getRooms()) {
             JCheckBox c = new JCheckBox();
             zones.add(c);
             c.setHorizontalAlignment(SwingConstants.CENTER);
@@ -94,44 +95,43 @@ public class SimParameterGUI extends JFrame{
         tempField.setHorizontalAlignment(SwingConstants.CENTER);
         zoneCreationPanel.add(tempField);
 
-        JComboBox<String> typesOfZones = new JComboBox<>(new String[]{"HEATING", "COOLING"});
+        JComboBox<String> typesOfZones = new JComboBox<>(new String[] { "HEATING", "COOLING" });
         zoneCreationPanel.add(typesOfZones);
         typesOfZonesList.add(typesOfZones);
 
+        // //for spacing
+        // JLabel empty3 = new JLabel("");
+        // zoneCreationPanel.add(empty3);
+        // empty3.setVisible(false);
+        // this button will add rows as needed
+        // JButton addZoneButton = new JButton("Add Zone");
+        // addZoneButton.addActionListener(new ActionListener() {
+        // @Override
+        // public void actionPerformed(ActionEvent e) {
+        // if(zoneCount < controller.getRooms().size()){
+        // addZones();
+        // }else{
+        // System.out.println("cannot make more room then zones");
+        // }
+        //
+        // }
+        // });
+        // buttonsPanel.add(addZoneButton);
 
-//        //for spacing
-//        JLabel empty3 = new JLabel("");
-//        zoneCreationPanel.add(empty3);
-//        empty3.setVisible(false);
-        //this button will add rows as needed
-//        JButton addZoneButton = new JButton("Add Zone");
-//        addZoneButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                if(zoneCount < controller.getRooms().size()){
-//                    addZones();
-//                }else{
-//                    System.out.println("cannot make more room then zones");
-//                }
-//
-//            }
-//        });
-//        buttonsPanel.add(addZoneButton);
-
-//        JButton doneZoneButton = new JButton("Done");
-//        doneZoneButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                listOfZones.add(zones);
-//                controller.setZones(listOfZones);
-//                controller.setTemperatureFile(filenameField.getText());
-//            }
-//        });
-//        buttonsPanel.add(doneZoneButton);
+        // JButton doneZoneButton = new JButton("Done");
+        // doneZoneButton.addActionListener(new ActionListener() {
+        // @Override
+        // public void actionPerformed(ActionEvent e) {
+        // listOfZones.add(zones);
+        // controller.setZones(listOfZones);
+        // controller.setTemperatureFile(filenameField.getText());
+        // }
+        // });
+        // buttonsPanel.add(doneZoneButton);
 
         mainZonePanel.add(zoneCreationPanel, BorderLayout.CENTER);
         add(panel1);
-        //setSize(600,300);
+        // setSize(600,300);
         setUPDateSelection();
         pack();
         setTitle("Simulation Parameters");
@@ -140,35 +140,36 @@ public class SimParameterGUI extends JFrame{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    private void setUpButtons(){
+    private void setUpButtons() {
         addZoneButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (zoneCount < controller.getRooms().size()){
-                    //listOfZones.add(zones);
+                if (zoneCount < controller.getRooms().size()) {
+                    // listOfZones.add(zones);
                     addZones();
-                }else{
+                } else {
                     System.out.println("maximum number of zones created");
                 }
 
             }
         });
 
-//        doneZoneButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                listOfZones.add(zones);
-//                controller.setZones(listOfZones,temperatures,typesOfZonesList);
-//            }
-//        });
+        // doneZoneButton.addActionListener(new ActionListener() {
+        // @Override
+        // public void actionPerformed(ActionEvent e) {
+        // listOfZones.add(zones);
+        // controller.setZones(listOfZones,temperatures,typesOfZonesList);
+        // }
+        // });
 
         doneButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 listOfZones.add(zones);
-                controller.setZones(listOfZones,temperatures,typesOfZonesList);
+                controller.setZones(listOfZones, temperatures, typesOfZonesList);
 
-                controller.setSimulationParams(filenameField.getText(), (Date) datePicker.getModel().getValue(), (int) hourSpinner.getModel().getValue(),
+                controller.setSimulationParams(filenameField.getText(), (Date) datePicker.getModel().getValue(),
+                        (int) hourSpinner.getModel().getValue(),
                         (int) minuteSpinner.getModel().getValue(), (int) tempSpinner.getValue(), profile);
 
                 Dashboard dash = new Dashboard(profile);
@@ -178,35 +179,34 @@ public class SimParameterGUI extends JFrame{
         });
     }
 
-    private void setUPDateSelection(){
-        SpinnerNumberModel spinHour = new SpinnerNumberModel(0,0,23,1);
-        SpinnerNumberModel spinMin = new SpinnerNumberModel(0,0,59,1);
+    private void setUPDateSelection() {
+        SpinnerNumberModel spinHour = new SpinnerNumberModel(0, 0, 23, 1);
+        SpinnerNumberModel spinMin = new SpinnerNumberModel(0, 0, 59, 1);
         hourSpinner.setModel(spinHour);
         minuteSpinner.setModel(spinMin);
 
-
         UtilDateModel model = new UtilDateModel();
-        model.setDate(2022,12,30);
+        model.setDate(2022, 12, 30);
         Properties p = new Properties();
         p.put("text.today", "Today");
         p.put("text.month", "Month");
         p.put("text.year", "Year");
 
-        JLabel l22=new JLabel("DATE :");
-        l22.setBounds(100,350,100,20);
+        JLabel l22 = new JLabel("DATE :");
+        l22.setBounds(100, 350, 100, 20);
 
-        JDatePanelImpl datePanel = new JDatePanelImpl(model,p);
+        JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
         datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
         add(datePicker, BorderLayout.SOUTH);
 
-
         testPanel.add(l22);
 
-        datePicker.setBounds(220,350,120,30);
+        datePicker.setBounds(220, 350, 120, 30);
         testPanel.add(datePicker);
         dateAndtimePanel.add(testPanel);
 
     }
+
     private void addZones() {
         zoneCount++;
         listOfZones.add(zones);
@@ -227,26 +227,25 @@ public class SimParameterGUI extends JFrame{
         tempField.setHorizontalAlignment(SwingConstants.CENTER);
         zoneCreationPanel.add(tempField);
 
-        JComboBox<String> typesOfZones = new JComboBox<>(new String[]{"HEATING", "COOLING"});
+        JComboBox<String> typesOfZones = new JComboBox<>(new String[] { "HEATING", "COOLING" });
         zoneCreationPanel.add(typesOfZones);
         typesOfZonesList.add(typesOfZones);
 
-//        JLabel empty1 = new JLabel("");
-//        zoneCreationPanel.add(empty1);
-//        empty1.setVisible(false);
+        // JLabel empty1 = new JLabel("");
+        // zoneCreationPanel.add(empty1);
+        // empty1.setVisible(false);
 
-//        JLabel empty2 = new JLabel("");
-//        zoneCreationPanel.add(empty2);
-//        empty2.setVisible(false);
+        // JLabel empty2 = new JLabel("");
+        // zoneCreationPanel.add(empty2);
+        // empty2.setVisible(false);
 
         validate();
         pack();
         repaint();
     }
 
-
     public static void main(String[] args) {
-        new SimParameterGUI(null,null);
+        new SimParameterGUI(null, null);
     }
 }
 
